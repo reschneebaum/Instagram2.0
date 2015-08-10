b4b2e2b0d839f4d682f9f90f2ab6ad39f1ca49fa
Rachel Schneebaum <reschneebaum@gmail.com>
2015-08-10 15:43:36 -0500

    Initial Commit

---
+++ Instagram2.0/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Instagram2.0
+//
+//  Created by Rachel Schneebaum on 8/10/15.
+//  Copyright (c) 2015 Rachel Schneebaum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Instagram2.0/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Instagram2.0
+//
+//  Created by Rachel Schneebaum on 8/10/15.
+//  Copyright (c) 2015 Rachel Schneebaum. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
