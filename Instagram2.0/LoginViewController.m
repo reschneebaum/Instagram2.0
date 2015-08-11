9d742130723a9fd8565828c7ec1c40d93acd75d4
Rachel Schneebaum <reschneebaum@gmail.com>
2015-08-11 12:40:52 -0500

    set while loop

--- Instagram2.0/LoginViewController.m
+++ Instagram2.0/LoginViewController.m
@@ -23,9 +23,6 @@
 
 @implementation LoginViewController
 
-// Quick change.
-// Another change.
-
 - (void)viewDidLoad {
     [super viewDidLoad];
 
@@ -46,48 +43,53 @@
 
 - (IBAction)onLoginButtonPressed:(UIButton *)sender {
 //    if (self.users.count > 0) {
-        for (User *user in self.users) {
-            while ([user.username isEqualToString:self.usernameTextField.text] && [user.password isEqualToString:self.passwordTextField.text]) {
-                self.areUsers = true;
-            }
-            if (self.areUsers == true) {
-                NSLog(@"go to profile page!");
-            } else {
-                NSLog(@"go to signup page!");
-                UIAlertController *wrongAccountAlert = [UIAlertController alertControllerWithTitle:@"Create a new account" message:nil preferredStyle:UIAlertControllerStyleAlert];
-                [wrongAccountAlert addTextFieldWithConfigurationHandler:^(UITextField *textField) {
+    for (User *user in self.users) {
+        while (!([user.username isEqualToString:self.usernameTextField.text] && [user.password isEqualToString:self.passwordTextField.text])) {
+            self.areUsers = false;
+
+            if (user == self.users.lastObject)
+                break;
+        }
+    }
+
+        if (self.areUsers == true) {
+            NSLog(@"go to profile page!");
+        } else {
+            NSLog(@"go to signup page!");
+            UIAlertController *wrongAccountAlert = [UIAlertController alertControllerWithTitle:@"Create a new account" message:nil preferredStyle:UIAlertControllerStyleAlert];
+            [wrongAccountAlert addTextFieldWithConfigurationHandler:^(UITextField *textField) {
                     textField.placeholder = @"First Name";
-                }];
-                [wrongAccountAlert addTextFieldWithConfigurationHandler:^(UITextField *textField) {
+            }];
+            [wrongAccountAlert addTextFieldWithConfigurationHandler:^(UITextField *textField) {
                     textField.placeholder = @"Last Name";
-                }];
-                [wrongAccountAlert addTextFieldWithConfigurationHandler:^(UITextField *textField) {
+            }];
+            [wrongAccountAlert addTextFieldWithConfigurationHandler:^(UITextField *textField) {
                     textField.placeholder = @"Username";
-                }];
-                [wrongAccountAlert addTextFieldWithConfigurationHandler:^(UITextField *textField) {
+            }];
+            [wrongAccountAlert addTextFieldWithConfigurationHandler:^(UITextField *textField) {
                     textField.placeholder = @"Password";
-                    textField.secureTextEntry = true;
-                }];
-                UIAlertAction *addAction = [UIAlertAction actionWithTitle:@"Add" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
-                    UITextField *firstNameTextField = ((UITextField *)[wrongAccountAlert.textFields objectAtIndex:0]);
-                    UITextField *lastNameTextField = ((UITextField *)[wrongAccountAlert.textFields objectAtIndex:1]);
-                    UITextField *usernameTextField = ((UITextField *)[wrongAccountAlert.textFields objectAtIndex:2]);
-                    UITextField *passwordTextField = ((UITextField *)[wrongAccountAlert.textFields objectAtIndex:3]);
-                    NSManagedObject *user = [NSEntityDescription insertNewObjectForEntityForName:@"User" inManagedObjectContext:self.moc];
-                    [user setValue:firstNameTextField.text forKey:@"firstName"];
-                    [user setValue:lastNameTextField.text forKey:@"lastName"];
-                    [user setValue:usernameTextField.text forKey:@"username"];
-                    [user setValue:passwordTextField.text forKey:@"password"];
-                    [self.moc save:nil];
-                    [self loadUsers];
-                }];
-                UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
-                }];
-                [wrongAccountAlert addAction:addAction];
-                [wrongAccountAlert addAction:cancelAction];
-                [self presentViewController:wrongAccountAlert animated:true completion:nil];
-            }
+                textField.secureTextEntry = true;
+            }];
+            UIAlertAction *addAction = [UIAlertAction actionWithTitle:@"Add" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+                UITextField *firstNameTextField = ((UITextField *)[wrongAccountAlert.textFields objectAtIndex:0]);
+                UITextField *lastNameTextField = ((UITextField *)[wrongAccountAlert.textFields objectAtIndex:1]);
+                UITextField *usernameTextField = ((UITextField *)[wrongAccountAlert.textFields objectAtIndex:2]);
+                UITextField *passwordTextField = ((UITextField *)[wrongAccountAlert.textFields objectAtIndex:3]);
+                NSManagedObject *user = [NSEntityDescription insertNewObjectForEntityForName:@"User" inManagedObjectContext:self.moc];
+                [user setValue:firstNameTextField.text forKey:@"firstName"];
+                [user setValue:lastNameTextField.text forKey:@"lastName"];
+                [user setValue:usernameTextField.text forKey:@"username"];
+                [user setValue:passwordTextField.text forKey:@"password"];
+                [self.moc save:nil];
+                [self loadUsers];
+            }];
+            UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
+            }];
+            [wrongAccountAlert addAction:addAction];
+            [wrongAccountAlert addAction:cancelAction];
+            [self presentViewController:wrongAccountAlert animated:true completion:nil];
         }
 }
+
 
 @end
