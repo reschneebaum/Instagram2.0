aced71ae567a644b3bd22721065f35689d31df6e
Edil Ashimov <edilashimov@Edils-MacBook-Pro.local>
2015-08-11 15:52:27 -0500

    detailProfileImageView is set

---
+++ Instagram2.0/DetailProfileImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailProfileImageViewController.h
+//  Instagram2.0
+//
+//  Created by Edil Ashimov on 8/11/15.
+//  Copyright (c) 2015 Rachel Schneebaum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailProfileImageViewController : UIViewController
+
+@end
