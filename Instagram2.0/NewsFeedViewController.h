c901e9b0c3ef86df10d432e9b34df7e871efaac5
Rachel Schneebaum <reschneebaum@gmail.com>
2015-08-11 12:19:34 -0500

    configure storyboard

---
+++ Instagram2.0/NewsFeedViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewsFeedViewController.h
+//  Instagram2.0
+//
+//  Created by Rachel Schneebaum on 8/11/15.
+//  Copyright (c) 2015 Rachel Schneebaum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewsFeedViewController : UIViewController
+
+@end
---
+++ Instagram2.0/SearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.h
+//  Instagram2.0
+//
+//  Created by Rachel Schneebaum on 8/11/15.
+//  Copyright (c) 2015 Rachel Schneebaum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController
+
+@end
